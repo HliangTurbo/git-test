4986eb2a0426d3ee98860fb9b153c2ac059994b8
Hliang_- <2406225241@qq,com>
2021-04-28 22:36:27 +0800

    idea second commit

--- src/main/java/com/atguigu/test/HelloGit.java
+++ src/main/java/com/atguigu/test/HelloGit.java
@@ -7,6 +7,7 @@ package com.atguigu.test;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        System.out.println("Hello,Git2!");
     }
     
 }
