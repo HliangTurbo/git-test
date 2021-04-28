bb10f3d57970169be55233b0828ef9346df50761
Hliang_- <2406225241@qq,com>
2021-04-28 22:26:08 +0800

    idea first commit

---
+++ src/main/java/com/atguigu/test/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.test;
+
+/**
+ * @author Hliang
+ * @create 2021-04-28 11:53
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+    }
+    
+}
